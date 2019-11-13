bb5abe4b1f7dc5c7a53d5c6c12d8909f5c0ab00d
Luana Faria Lane <luanasfff@gmail.com>
2019-11-13 04:12:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ end
 
 def assign_rooms(speakers)
  speakers.map.with_index do |name, index|
-   puts "Hello, #{name}! You'll be assigned to room #{index +1}!"
+   puts "Hello,#{name}! You'll be assigned to room #{index+1}!"
   end 
 end
 
